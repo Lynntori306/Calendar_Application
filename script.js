48efeeeff35c6fe0732335ecbf69a3b59da3ff1f
Tori McHenry <lynntorim@live.com>
2020-04-04 22:55:50 -0700

    JS edit

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-
-let date;
-date = new Date();
-document.getElementById("currentDay").innerHTML = Date();
-
+//date done
+var tDate = new Date();
+document.getElementById("currentDay").innerHTML = tDate.toLocaleString();
 
 
-var container = document.getElementById("container");
-//var row = container.insertRow()
-   
+//local storage set to text box when you click button
+var saveButton = document.querySelector("saveBtn");
+var Input = document.querySelector("#userInput");
+
+var save = localStorage.getItem("save");
+
+userInput.textContent = "save";
+
+saveButton.addEventListener("click", function (){
+    localStorage.setItem("save", save);
+    alert("Text saved");
+});
+
+
+//change color of rows with the time
+
 
